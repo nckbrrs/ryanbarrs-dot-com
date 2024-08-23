2333360347627b7235d16c110a02a14481106b84
nckbrrs <github@nickbarrs.com>
2024-08-22 22:53:49 -0400

    lower opacity of footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,8 +5,8 @@ const Footer: React.FC = () => {
     return (
         <Col tw="pb-4 pt-6 sm:pb-10 md:pb-12 lg:pb-20 justify-end items-end">
             <Row>
-            <p tw="text-[0.75em] font-mono tracking-tightest text-bone opacity-50">made with ❤️ by&nbsp;</p>
-            <a tw="text-[0.75em] font-mono tracking-tightest text-bone opacity-50 hover:opacity-100 duration-100" href="https://nickbarrs.com/" target="_blank" rel="noopener noreferrer">Nick Barrs</a>
+            <p tw="text-[0.75em] font-mono tracking-tightest text-bone opacity-30">made with ❤️ by&nbsp;</p>
+            <a tw="text-[0.75em] font-mono tracking-tightest text-bone opacity-30 hover:opacity-80 duration-100" href="https://nickbarrs.com/" target="_blank" rel="noopener noreferrer">Nick Barrs</a>
             </Row>
         </Col>
     )
