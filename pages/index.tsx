750f7a7f186fd6f3b27fc4ae744e3bc3df88446b
nckbrrs <github@nickbarrs.com>
2024-08-22 22:53:40 -0400

    tweak "hi i'm ryan" drop shadow on mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ const Home: React.FC<{users: any}> = ({ users }) => (
       <title>Ryan Barrs / Home</title>
     </Head>
     <Col tw="pt-12 h-full justify-center items-center space-y-10 md:space-y-12">
-      <p tw="text-white text-4xl md:text-7xl tracking-wide font-canelaDeck-Bold font-[600] drop-shadow-[0px 3px 4px black]">Hi, I'm Ryan.</p>
+      <p tw="text-white text-4xl md:text-7xl tracking-wide font-canelaDeck-Bold font-[600] drop-shadow-[0px 1px 2px black] lg:drop-shadow-[0px 3px 4px black]">Hi, I'm Ryan.</p>
       <p tw="w-fit text-center text-white tracking-wide font-canelaDeck text-2xl md:text-5xl xl:text-6xl drop-shadow-[0px 2px 2px black]">I am fixing broken hearts with bioengineering.</p>
       <p tw="w-fit text-center text-white tracking-wide font-canelaDeck text-2xl md:text-5xl xl:text-6xl drop-shadow-[0px 2px 2px black]">Let's work together.</p>
     </Col>
