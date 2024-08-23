5ad559b1963bb92d63ddc1fa5610989f16e66f00
nckbrrs <github@nickbarrs.com>
2024-08-22 23:01:19 -0400

    tweak padding so home screen text appears below iceberg breach

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ const Home: React.FC<{users: any}> = ({ users }) => (
     <Head>
       <title>Ryan Barrs / Home</title>
     </Head>
-    <Col tw="pt-12 h-full justify-center items-center space-y-10 md:space-y-12">
+    <Col tw="pt-24 h-full justify-center items-center space-y-10 md:space-y-12">
       <p tw="text-white text-4xl sm:text-5xl md:text-7xl tracking-wide font-canelaDeck-Bold font-[600] drop-shadow-[0px 1px 2px black] md:drop-shadow-[0px 3px 4px black]">Hi, I'm Ryan.</p>
       <p tw="w-fit text-center text-white tracking-wide font-canelaDeck text-2xl sm:text-4xl md:text-5xl xl:text-6xl drop-shadow-[0px 2px 2px black]">I am fixing broken hearts with bioengineering.</p>
       <p tw="w-fit text-center text-white tracking-wide font-canelaDeck text-2xl sm:text-4xl md:text-5xl xl:text-6xl drop-shadow-[0px 2px 2px black]">Let's work together.</p>
