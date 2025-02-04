3e61632fa73590f01f18b8ee5f901359cedfb9a4
nckbrrs <github@nickbarrs.com>
2025-02-04 18:28:04 -0500

    add DATABASE_URL back to env validation

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,10 +3,9 @@ import { type Config } from "drizzle-kit";
 import { env } from "~/env";
 
 export default {
-  schema: "./src/server/db/schema.ts",
-  dialect: "postgresql",
-  dbCredentials: {
-    url: env.DATABASE_URL,
-  },
-  tablesFilter: ["production-painting-decorating-inc_*"],
+	schema: "./src/server/db/schema.ts",
+	dialect: "postgresql",
+	dbCredentials: {
+		url: env.DATABASE_URL
+	}
 } satisfies Config;
